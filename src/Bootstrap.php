cc0e7b409e007269efec993cc75b9f845543e742
pinelopinastou <penelope_nastou@hotmail.com>
2020-09-03 19:45:00 +0300

    Init bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR . '/vendor/autoload.php';
+\Tracy\Debugger::enable();
+
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$content = 'Hello ' . $request->get('name', 'visitor');
+$response = new \Symfony\Component\HttpFoundation\Response($content); $response->prepare($request);
+$response->send();
